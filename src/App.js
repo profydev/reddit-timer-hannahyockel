b6b46a0fa6f022e6a0fd57bbf38c5dc1ff8bd5f4
hannahyockel <88843058+hannahyockel@users.noreply.github.com>
2022-01-13 16:32:07 -0600

    feat(core): create app skeleton
    > 1. Install fonts Bitter and Monsterrat > 2. Add CSS reset (normalize.css > 3. Add global styles > 4. Add React Router with Routes '/' and '/search' > 5. Setup initial folder structure and import marketing page images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import sign from './img/sign.svg';
+import table from './img/table.png';
 
 function App() {
   return (
     <div>
-      App Placeholder
+      <div className="Content">
+        <h1>No reactions to your Reddit posts?</h1>
+        <p>
+          Great timing, great results! Find the best time to post on your
+          subreddit.
+        </p>
+        <button type="button">SHOW ME THE BEST TIME</button>
+        <p>r/javascript</p>
+        <img src={table} alt="Reddit timer example" />
+        <h2 id="how-it-works">How it works</h2>
+        <ul>
+          <li>We find the 500 top posts from the past year for a subreddit.</li>
+          <li>
+            The data is visualized in a heatmap grouped by weekday and hour of
+            the day.
+          </li>
+          <li>See immediately when to submit your reddit post.</li>
+        </ul>
+        <h2 id="about">About</h2>
+        <p>
+          This app was created during a course on
+          {' '}
+          <a href="https://profy.dev/">profy.dev</a>
+          {' '}
+          with the goal to implement a pixel-perfect real-world application with
+          professional workflows and tools like Kanban, Asana, Zeplin, GitHub,
+          pull requests and code reviews.
+          {' '}
+          <a href="https://profy.dev/">Click here for more information.</a>
+        </p>
+      </div>
+      <footer>
+        <a href="/">profy.dev</a>
+        <a href="/">
+          <img src={sign} alt="Sign" />
+        </a>
+        <a href="/">Terms & Privacy</a>
+      </footer>
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../img/logo.svg';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Link to="/">
+        <img src={logo} alt="Logo" />
+      </Link>
+      <Link to="/search">Search</Link>
+      <a href="#how-it-works">How it works</a>
+      <a href="#about">About</a>
+    </nav>
+  );
+}
