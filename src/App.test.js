166577f4dc168ba94f30f4f213ab192649755521
hannahyockel <88843058+hannahyockel@users.noreply.github.com>
2022-01-13 22:55:54 -0600

    Reset app tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,28 +1,7 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import { MemoryRouter } from 'react-router-dom';
-// import userEvent from '@testing-library/user-event';
-// import App from './App';
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
 
-// function setup() {
-//   return render(
-//     <MemoryRouter>
-//       <App />
-//     </MemoryRouter>,
-//   );
-// }
-
-// describe('Header', () => {
-//   test('Nav links points to the correct page', () => {
-//     setup();
-//     const aboutLink = screen.getByRole('link', { name: /about/i });
-//     userEvent.click(aboutLink);
-
-//     expect(aboutLink).toBeInTheDocument();
-
-//     const HowItWorks = screen.getByRole('link', { name: /how it works/i });
-//     userEvent.click(HowItWorks);
-
-//     expect(HowItWorks).toBeInTheDocument();
-//   });
-// });
+test('renders App', () => {
+  render(<App />);
+});
