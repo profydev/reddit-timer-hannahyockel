7a311648478cf6a06de8aed291b8376e7778a9aa
Hannah <88843058+hannahyockel@users.noreply.github.com>
2022-01-13 23:08:19 -0600

    feat(core): create app skeleton (#1)
    * feat(core): create app skeleton
    
    > 1. Install fonts Bitter and Monsterrat > 2. Add CSS reset (normalize.css > 3. Add global styles > 4. Add React Router with Routes '/' and '/search' > 5. Setup initial folder structure and import marketing page images
    
    * Updates title, meta, and names in /public
    
    * Updates favicon from CRA default to Reddit Timer logo
    
    * Uses a common file structure
    Uses yarn instead of npm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Navbar from './Components/Navbar';
+import './index.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
