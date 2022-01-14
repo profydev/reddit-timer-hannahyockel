7015cac258666c261f0759bc77bb6deedf3b4c2d
hannahyockel <88843058+hannahyockel@users.noreply.github.com>
2022-01-13 18:02:57 -0600

    Update this branch to reflect changes in header and footer

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -4,19 +4,19 @@ import table from '../img/table.png';
 
 export default function Hero() {
   return (
-    <div className="Content">
+    <header>
       <h1>No reactions to your Reddit posts?</h1>
       <p>
         Great timing, great results! Find the best time to post on your
         subreddit.
       </p>
-      <Link to="/search">
+      <Link to="/search/javascript">
         <button type="button">SHOW ME THE BEST TIME</button>
       </Link>
-      <Link to="/search">
+      <Link to="/search/javascript">
         <p>r/javascript</p>
       </Link>
       <img src={table} alt="Reddit timer example" />
-    </div>
+    </header>
   );
 }
