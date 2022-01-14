8773b28fd62f9cef781c5749c8eb7263ff0f2123
hannahyockel <88843058+hannahyockel@users.noreply.github.com>
2022-01-13 18:45:49 -0600

    Add 2 sections: How it works and About to new component: InfoSection

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import React from 'react';
+import InfoSection from './Components/InfoSection';
 
 function App() {
   return (
     <div>
-      <h2 id="how-it-works">How it works</h2>
-      <ul>
-        <li>We find the 500 top posts from the past year for a subreddit.</li>
-        <li>
-          The data is visualized in a heatmap grouped by weekday and hour of
-          the day.
-        </li>
-        <li>See immediately when to submit your reddit post.</li>
-      </ul>
-      <h2 id="about">About</h2>
-      <p>
-        This app was created during a course on
-        {' '}
-        <a href="https://profy.dev/">profy.dev</a>
-        {' '}
-        with the goal to implement
-        a pixel-perfect real-world application with professional workflows and
-        tools like Kanban, Asana, Zeplin, GitHub, pull requests and code
-        reviews.
-        {' '}
-        <a href="https://profy.dev/">Click here for more information.</a>
-      </p>
+      <InfoSection />
     </div>
   );
 }
