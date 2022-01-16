e8904a690dcba160226e9df03759a5cb7925b7ed
hannahyockel <88843058+hannahyockel@users.noreply.github.com>
2022-01-15 18:58:48 -0600

    Adds skeleton files for style/ theme and global style.
    Adds proper packages to App
    
    Adds theme provider, global style, header, switch and its routes (search and home) to App

--- src/App.js
+++ src/App.js
@@ -1,49 +1,23 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import sign from './img/sign.svg';
-import table from './img/table.png';
+import { Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import { GlobalStyle, theme } from './style';
+import Header from './common/header';
 
 function App() {
   return (
-    <div>
-      <div className="Content">
-        <h1>No reactions to your Reddit posts?</h1>
-        <p>
-          Great timing, great results! Find the best time to post on your
-          subreddit.
-        </p>
-        <button type="button">SHOW ME THE BEST TIME</button>
-        <p>r/javascript</p>
-        <img src={table} alt="Reddit timer example" />
-        <h2 id="how-it-works">How it works</h2>
-        <ul>
-          <li>We find the 500 top posts from the past year for a subreddit.</li>
-          <li>
-            The data is visualized in a heatmap grouped by weekday and hour of
-            the day.
-          </li>
-          <li>See immediately when to submit your reddit post.</li>
-        </ul>
-        <h2 id="about">About</h2>
-        <p>
-          This app was created during a course on
-          {' '}
-          <a href="https://profy.dev/">profy.dev</a>
-          {' '}
-          with the goal to implement a pixel-perfect real-world application with
-          professional workflows and tools like Kanban, Asana, Zeplin, GitHub,
-          pull requests and code reviews.
-          {' '}
-          <a href="https://profy.dev/">Click here for more information.</a>
-        </p>
-      </div>
-      <footer>
-        <a href="/">profy.dev</a>
-        <a href="/">
-          <img src={sign} alt="Sign" />
-        </a>
-        <a href="/">Terms & Privacy</a>
-      </footer>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Normalize />
+      <GlobalStyle />
+      <Header />
+      <Switch>
+        <Route path="/search">Search Page</Route>
+        <Route path="/">Home</Route>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
---
+++ src/style/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+  color: {
+    text: '#93918f',
+    dark: '#000000',
+    midDark: '#636363',
+    midLight: '#d5d5d5',
+    light: '#ffffff',
+  },
+};
+
+export default theme;
