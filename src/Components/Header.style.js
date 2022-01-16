8447fb7077552ce2d013450871d3ce41b88f1308
hannahyockel <88843058+hannahyockel@users.noreply.github.com>
2022-01-15 18:32:44 -0600

    Adds skeleton of Header styled components

---
+++ src/Components/Header.style.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/prefer-default-export */
+import styled from 'styled-components';
+import { Link as UnstyledLink } from 'react-router-dom';
+import { ReactComponent as UnstyledLogo } from './logo.svg';
+
+export const Container = styled.header`
+  width: 100%;
+`;
+
+export const Logo = styled(UnstyledLogo)`
+  margin-top: 0.5rem;
+`;
+
+export const Link = styled(UnstyledLink)`
+
+`;
