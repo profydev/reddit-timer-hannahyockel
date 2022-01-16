71a4b90e89051f5ac8428e38d5d68c1f39ba0d6f
hannahyockel <88843058+hannahyockel@users.noreply.github.com>
2022-01-15 18:22:29 -0600

    Restructures the header component's HTML

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../img/logo.svg';
 
-export default function Header() {
+function Header() {
   return (
     <header>
       <Link to="/">
         <img src={logo} alt="Logo" />
       </Link>
-      <Link to="/search/javascript">Search</Link>
-      <a href="#how-it-works">How it works</a>
-      <a href="#about">About</a>
+      <nav>
+        <Link to="/search/javascript">Search</Link>
+        <a href="/#how-it-works">How it works</a>
+        <a href="/#about">About</a>
+      </nav>
     </header>
   );
 }
+
+export default Header;
